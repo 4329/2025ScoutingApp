4f308530e66ae1f52840cc474787ed88cc2730d1
alphaom33 <alphaom34@gmail.com>
2025-04-16 12:37:14 -0500

    ?

--- app/ui/NavPanel.jsx
+++ app/ui/NavPanel.jsx
@@ -4,8 +4,11 @@ export default function NavPanel() {
     const [burger, setBurger] = useState(false);
     return (
         <nav onBlur={() => setTimeout(() => setBurger(false), 300)} className={`relative z-10 ${burger ? "-mb-[276px] bg-[#333]" : "-mb-[60px]"} w-[300px]`}>
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <button onClick={() => setBurger(!burger)}data-collapse-toggle="navbar-hamburger" type="button" className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
+            <button onClick={e => {
+                e.target.focus();
+                setBurger(!burger);
+            }}data-collapse-toggle="navbar-hamburger" type="button" className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
